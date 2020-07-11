e176239217d1a1625fce34b5955bf1e384158616
Sima <sima.boreiri@gmail.com>
2020-07-11 18:07:41 +0200

    modifies some indentations and removes extra lines

--- spec/requests/api/v1/user_can_update_order_spec.rb
+++ spec/requests/api/v1/user_can_update_order_spec.rb
@@ -9,13 +9,12 @@ RSpec.describe 'PUT /api/v1/orders', type: :request do
     let(:order) { create(:order, user: user) }
     let!(:order_item) { create(:order_item, product: pizza, order: order) }
 
-  
     describe 'successfully' do
-        before do 
-        put "/api/v1/orders/#{order.id}",
-        params: { product_id: mushrooms.id },
-        headers: user_headers
-       end
+      before do 
+       put "/api/v1/orders/#{order.id}",
+       params: { product_id: mushrooms.id },
+       headers: user_headers
+      end
 
       it 'responds with 200 status' do
         expect(response.status).to eq 200
